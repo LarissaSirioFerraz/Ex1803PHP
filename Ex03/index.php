519e1413ace7ad4a048521c038183969b2744cab
EduardaMarinsBrandao <duda13brandao@gmail.com>
2025-03-18 16:42:19 -0300

    Inserir valor gasto da compra e a opção de pagar a prazo ou a vista no index.php

---
+++ Ex03/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE HTML>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>Valor desconto</title>
+</head>
+<body>
+<fieldset>
+    <legend>CALCULAR</legend>
+    <form method="post" action="calcular.php">
+        Informe o total gasto da compra:
+        <input type="number" name="n1" size="25" /><br />
+        Digite 1 para pagar a vista e 2 para pagar a prazo:
+        <input type="number" name="x" size="25" /><br />
+
+		<button type="submit">Próximo</button>
+        <br />
+    </form>
+</fieldset>
+</body>
+</html>
